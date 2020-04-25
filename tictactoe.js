03bbc036dd8e8ef4b8f7ae2e7415c8573398e04c
claudia <mail@claudiapiccioni.it>
2020-04-25 11:35:44 +0200

    determined whose turn it is and then which relative class to use for my e.target cell (the one I clicked). I can work now in order to place my marks

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,18 @@
-const cellElements = document.querySelectorAll("[data-cell]")
+const X_CLASS = "x"
+const CIRCLE_CLASS = "circle"
 
+const cellElements = document.querySelectorAll("[data-cell]")
+let circleTurn
 
 cellElements.forEach((cell) => {
   cell.addEventListener("click", handleClick, { once: true })
 })
 
 function handleClick(e) {
-  console.log("clicked")
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  // placeMark
+  // Check For Win
+  // Check For Draw
+  // Switch Turns
 }
