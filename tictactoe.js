816e3bf5e935b73b2fa98cc37be3fed5ccf7cf8d
claudia <mail@claudiapiccioni.it>
2020-04-25 14:16:11 +0200

    easily created the placeMark function adding the currentClass to the clicked cell. Now I need to work on how to switch turn

--- tictactoe.js
+++ tictactoe.js
@@ -4,15 +4,20 @@ const CIRCLE_CLASS = "circle"
 const cellElements = document.querySelectorAll("[data-cell]")
 let circleTurn
 
-cellElements.forEach((cell) => {
-  cell.addEventListener("click", handleClick, { once: true })
+cellElements.forEach((cellElement) => {
+  cellElement.addEventListener("click", handleClick, { once: true })
 })
 
 function handleClick(e) {
   const cell = e.target
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
   // placeMark
+  placeMark(cell, currentClass)
   // Check For Win
   // Check For Draw
   // Switch Turns
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
 }
